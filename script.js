4309cf5355a8888a5e70b0e4283f63816aa71993
jeeyalal <harshmishra6927@gmail.com>
2025-01-20 17:50:50 +0530

    Updated the files

--- script.js
+++ script.js
@@ -47,5 +47,21 @@ gsap.from("#page1 #video-con",{
     
 })
 
+document.addEventListener("mousemove",function(dets){
+     gsap.to("#cursor",{
+        left:dets.x,
+        top:dets.y
+     })
+})
 
+document.querySelector('#child1').addEventListener("mouseenter",function(){
+    gsap.to("#cursor",{
+        transform: 'translate(-50%,-50%) scale(1)'
+    })
+})
 
+document.querySelector('#child1').addEventListener("mouseleave",function(){
+    gsap.to("#cursor",{
+        transform: 'translate(-50%,-50%) scale(0)'
+    })
+})
