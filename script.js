d8679b0d798375a2ac17579076825278d5c8d0ac
jeeyalal <harshmishra6927@gmail.com>
2025-01-20 14:11:36 +0530

    Add new files and update hindex.html

---
+++ script.js
@@ -0,0 +1,48 @@
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
+
+function videoAnimation() {
+    var videocon = document.querySelector("#video-con")
+    var playbutton = document.querySelector("#play")
+    
+    videocon.addEventListener("mouseenter", function(){
+        
+        gsap.to(playbutton,{
+            scale:1,
+            opacity:1
+        })
+    })
+    videocon.addEventListener("mouseleave",function(){
+        gsap.to(playbutton,{
+            scale:0,
+            opacity:0
+        })
+    
+    })
+    
+    videocon.addEventListener("mousemove",function(dets){
+        gsap.to(playbutton,{
+            left:dets.x,
+            top:dets.y
+        })
+    
+    })  
+}
+videoAnimation()
+
+gsap.from("#page1 h1",{
+    y:30,
+    opacity:0,
+    delay:0.5,
+    duration:0.5,
+    stagger:0.3
+})
+gsap.from("#page1 #video-con",{
+    scale:0.9,
+    opacity:0,
+    delay:1.2,
+    duration:0.5,
+    
+})
